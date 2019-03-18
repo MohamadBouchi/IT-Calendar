01292088db6f92b33ade398bf0dc3b8597de3280
mohamedsubhi <mohamedsubhibouchi@gmail.com>
2019-03-18 08:19:49 +0100

    vue config

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,11 @@
 module.exports = {
+    publicPath: process.env.NODE_ENV === 'production'
+    ? console.log(process.env.NODE_ENV+' prod')
+    : console.log(process.env.NODE_ENV+' dev'),
+    productionSourceMap: false,
     configureWebpack: {
-        devtool: false
+        devtool: false,
         // devtool: 'source-map'
     }
 }
 
-console.log(process.env.NODE_ENV);
